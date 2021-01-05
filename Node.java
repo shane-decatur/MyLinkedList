9191a1f518b837e7b7bd2ef0b653b5d6cac76715
shane-decatur <sdecatur20@stuy.edu>
2021-01-05 14:25:58 -0500

    added the get() methods

--- Node.java
+++ Node.java
@@ -1,6 +1,22 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
+ public Node(String value){
+   data = value;
+ }
  private String data;
  private Node next,prev;
+
+ public String getData(){
+   return data;
+ }
+ 
+
+ public String getNext(){
+   return next.getData();
+ }
+
+
+ public String getPrev(){
+   return prev.getData();
+ }
  //write get/set methods for all three instance variables.
 }
