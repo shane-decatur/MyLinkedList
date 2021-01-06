7eadb9aaa6c087381d7a4bfc2a4537e0e3a329a9
shane-decatur <sdecatur20@stuy.edu>
2021-01-06 17:03:26 -0500

    added size()

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,26 @@
 public class MyLinkedList{
  private int size;
  private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
- public boolean add(String value);
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+ public MyLinkedList(){
+   size = 0;
+ }
+ public int size(){
+   return size;
+ }
+ public boolean add(String value){
+   return false;
+ }
+ public void add(int index, String value){
+   
+ }
+ public String get(int index){
+   return null;
+ }
+ public String set(int index, String value){
+   return null;
+ }
+ public String toString(){
+   return null;
+ }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
