63191bebbca0eb530fd62c7b7b27ab34587c94ef
shane-decatur <sdecatur20@stuy.edu>
2021-01-06 19:07:20 -0500

    added add(int index, String value

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -23,7 +23,32 @@ public class MyLinkedList{
    return true;
  }
  public void add(int index, String value){
-
+   Node n = new Node(value);
+   Node in = findIndex(index);
+   if (size < 1){
+     start = n;
+     end = n;
+     size++;
+   }
+   else if (in == start){
+     n.setNext(start);
+     start.setPrev(n);
+     start = n;
+     size++;
+   }
+   else if (in == null){
+     end.setNext(n);
+     n.setPrev(end);
+     end = n;
+     size++;
+   }
+   else{
+     n.setNext(in);
+     n.setPrev(in.getPrev());
+     in.getPrev().setNext(n);
+     in.setPrev(n);
+     size++;
+   }
  }
  public String get(int index){
    return null;
@@ -41,6 +66,19 @@ public class MyLinkedList{
    }
    list = list+"]";
    return list;
+ }
+
+ private Node findIndex(int index){
+   if (index < 0 || index > size){
+     throw new IndexOutOfBoundsException();
+   }
+   Node current = start;
+   int i = 0;
+   while (i < index){
+     current = current.getNext();
+     i++;
+   }
+   return current;
  }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
